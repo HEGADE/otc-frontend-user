bcb86facc9cc8a5111040447ab953f4033cbc4f0
AJONCODE <ajoncode@gmail.com>
2024-03-18 12:59:17 +0530

    FIX : Fetching user details via auth token

--- src/utils/config/api-end-points.config.js
+++ src/utils/config/api-end-points.config.js
@@ -8,8 +8,8 @@ export const API = {
   verifyPhoneNumber: "auth/verify-otp",
   sendTokenForEmailVerification: "auth/send-verification-email",
   sendOtpForPhoneNumVerification: "auth/send-otp",
-  getUserDetails: (userId) => `users/${userId}`,
-  saveUserDetails: (userId) => `users/${userId}`,
+  getUserDetails: `users/auth-user-info`,
+  saveUserDetails: `users/auth-user-info`,
   getBankDetails: "bank",
   saveBankDetails: "bank",
 };
